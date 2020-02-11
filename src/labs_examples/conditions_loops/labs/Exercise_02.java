d69e1e699f4af6ea301bde7eb8ee142cf3fb3e01
Isadora <zara.ven@gmail.com>
2020-02-11 19:52:42 +0700

    Finised Exercises 01 to 03

--- src/labs_examples/conditions_loops/labs/Exercise_02.java
+++ src/labs_examples/conditions_loops/labs/Exercise_02.java
@@ -1,5 +1,7 @@
 package labs_examples.conditions_loops.labs;
 
+import java.util.Scanner;
+
 /**
  * Conditions and Loops Exercise 2: Days of the week
  *
@@ -13,9 +15,42 @@ public class Exercise_02 {
     public static void main(String[] args) {
 
         // 1) create scanner (don't forget to import Scanner!)
+        Scanner scanner = new Scanner(System.in);
         // 2) prompt user
+        System.out.print("Please enter a number between 1 and 7 inclusive: ");
         // 3) assign input to variable as int
+        int num = scanner.nextInt();
         // 4) write completed code here
+        if (num <= 7 & num > 0) {
+            if (num > 1) {
+                if (num > 2) {
+                    if (num > 3) {
+                        if (num > 4) {
+                            if (num > 5) {
+                                if (num > 6) {
+                                    System.out.println("Sunday");
+                                } else {
+                                    System.out.println("Saturday");
+                                }
+                            } else {
+                                System.out.println("Friday");
+                            }
+                        } else {
+                            System.out.println("Thursday");
+                        }
+                    } else {
+                        System.out.println("Wednesday");
+                    }
+                } else {
+                    System.out.println("Tuesday");
+                }
+            } else {
+                System.out.println("Monday");
+            }
+        } else {
+            System.out.println("Error");
+        }
 
     }
+
 }
