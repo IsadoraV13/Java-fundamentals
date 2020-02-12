230fb56ba06d8cd812f4edd7b88fc17efb49b95e
Isadora <zara.ven@gmail.com>
2020-02-12 09:59:17 +0700

    Updated Ex 04

--- src/labs_examples/conditions_loops/labs/Exercise_04.java
+++ src/labs_examples/conditions_loops/labs/Exercise_04.java
@@ -9,9 +9,10 @@ package labs_examples.conditions_loops.labs;
 
 public class Exercise_04 {
     public static void main(String[] args) {
-        for (int i = 2; i <= 100; i+=2) {
-            System.out.println(i);
+        for (int i = 1; i <= 100; i++) {
+            if (i % 2 == 0) {
+                System.out.println(i);
+            }
         }
     }
-
 }
