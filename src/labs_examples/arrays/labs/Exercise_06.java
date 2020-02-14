4f522919b981503eb0c3e857403d549d0d8cf5db
Isadora <zara.ven@gmail.com>
2020-02-14 08:45:17 +0700

    Finished Ex 06 with questions

--- src/labs_examples/arrays/labs/Exercise_06.java
+++ src/labs_examples/arrays/labs/Exercise_06.java
@@ -11,4 +11,12 @@ package labs_examples.arrays.labs;
  *
  */
 public class Exercise_06 {
+    public static void main(String[] args) {
+        int array [] = {1, 2, 3, 4, 5};
+        for (int j = array.length -1; j >= 0; j += -1) {
+            System.out.printf("%3d", array[j]);
+            // can the elements of the array be printed as is or must the output itself be an array?
+        }
+
+    }
 }
