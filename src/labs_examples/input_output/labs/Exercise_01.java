bf12975a28f273b2276f542e2aedcd0d0cfa208e
Isadora <zara.ven@gmail.com>
2020-03-04 17:30:28 +0800

    Finished Ex 01 and 02

--- src/labs_examples/input_output/labs/Exercise_01.java
+++ src/labs_examples/input_output/labs/Exercise_01.java
@@ -1,5 +1,11 @@
 package labs_examples.input_output.labs;
 
+
+import java.io.BufferedInputStream;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
 /**
  * Input/Output Exercise 1: File input/output
  *
@@ -10,9 +16,29 @@ package labs_examples.input_output.labs;
  */
 
 class Example {
+    static String filePath = "/Users/isadora/Documents/CodingNomads/DailyWork/ClassCode/Day_1_Quiz/Day_1_Quiz.txt";
     public static void main(String[] args) {
+        BufferedInputStream bin = null;
 
-
-
+        try {
+            bin = new BufferedInputStream(new FileInputStream(filePath));
+            byte[] buffer = new byte[5];
+            int c;
+            while((c = bin.read(buffer)) != -1) {
+                for(byte b : buffer) {
+                    System.out.print((char)b);
+                }
+            }
+        } catch (FileNotFoundException e) {
+            System.out.println("File Not Found");
+        } catch (IOException e) {
+            System.out.println("IOException");
+        } finally {
+            try {
+                bin.close(); // does this automatically also close the File input stream?
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
     }
 }
--- src/labs_examples/input_output/labs/Exercise_02.java
+++ src/labs_examples/input_output/labs/Exercise_02.java
@@ -1,5 +1,10 @@
 package labs_examples.input_output.labs;
 
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+
 /**
  * Input/Output Exercise 2: File encryption
  *
@@ -12,3 +17,62 @@ package labs_examples.input_output.labs;
  *
  */
 
+class Exercise_02 {
+    static String readFilePath = "/Users/isadora/Documents/CodingNomads/DailyWork/ClassCode/Day_1_Quiz/Day_1_Quiz.txt";
+    static String writeFilePath = "/Users/isadora/Documents/CodingNomads/DailyWork/ClassCode/Day_1_Quiz/Day_1_Quiz_Rewrite.txt";
+    public static void main(String[] args) {
+        FileReader fr = null;
+        FileWriter fw = null;
+        FileReader fr2 = null;
+
+        //Reading Quiz.txt and writing to Quiz_Rewrite.txt (with 'encryption')
+        try {
+            fr = new FileReader(readFilePath);
+            fw = new FileWriter(writeFilePath);
+            int c;
+            while((c = fr.read()) != -1) {
+                if (c == 97) {
+                    c = 45;
+                } else if (c == 101) {
+                    c = 126;
+                }
+                fw.write(c);
+            }
+        } catch (FileNotFoundException e) {
+            System.out.println("File Not Found");
+        } catch (IOException e) {
+            System.out.println("IOException");
+        } finally {
+            try {
+                fr.close();
+                fw.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+
+        //Reading Quiz_Rewrite.txt, 'decrypting' and printing to console
+        try {
+            fr2 = new FileReader(readFilePath);
+            int c;
+            while((c = fr2.read()) != -1) {
+                if (c == 45) {
+                    c = 97;
+                } else if (c == 126) {
+                    c = 101;
+                }
+                System.out.print((char)(c));
+            }
+        } catch (FileNotFoundException e) {
+            System.out.println("File Not Found");
+        } catch (IOException e) {
+            System.out.println("IOException");
+        } finally {
+            try {
+                fr2.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
