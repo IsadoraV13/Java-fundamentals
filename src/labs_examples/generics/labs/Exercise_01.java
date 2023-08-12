766839d54d64085090fe1b3d894fa4a229ee0033
IsadoraV13 <30869562+IsadoraV13@users.noreply.github.com>
2023-08-12 10:29:42 +0200

    added latest changes before merge

--- src/labs_examples/generics/labs/Exercise_01.java
+++ src/labs_examples/generics/labs/Exercise_01.java
@@ -8,3 +8,48 @@ package labs_examples.generics.labs;
  *          dynamic usage.
  */
 
+public class Exercise_01  {
+
+    public static void main(String[] args) {
+        SomeGeneric <String, Integer> item1 = new SomeGeneric<>("13", 13);
+        System.out.println((item1.getObject1().getClass().getSimpleName() == item1.getObject2().getClass().getSimpleName()));
+
+        SomeGeneric <Double, Integer> item2 = new SomeGeneric<>(13.0, 13);
+        System.out.println((item2.getObject1().getClass().getSimpleName() == item2.getObject2().getClass().getSimpleName()));
+
+    }
+
+}
+
+class SomeGeneric<T, N> {
+    private T object1;
+    private N object2;
+
+    public SomeGeneric(T object1, N object2) {
+        this.object1 = object1;
+        this.object2 = object2;
+    }
+
+    public T getObject1() {
+        return object1;
+    }
+
+    public void setObject1(T object1) {
+        this.object1 = object1;
+    }
+
+    public N getObject2() {
+        return object2;
+    }
+
+    public void setObject2(N object2) {
+        this.object2 = object2;
+    }
+
+}
+
+
+
+
+
+
--- src/labs_examples/generics/labs/Exercise_03.java
+++ src/labs_examples/generics/labs/Exercise_03.java
@@ -20,5 +20,30 @@ import java.util.Collection;
  */
 
 public class Exercise_03 {
+    public static void main(String[] args) {
+        System.out.println(sumCalculator(13, 13.45));
+        System.out.println(countPalindromes("Aviva"));
+    }
 
+public static <N extends Number, T extends Number> double sumCalculator (N num1, T num2){
+        double result = num1.doubleValue() + num2.doubleValue();
+        return result;
+    }
+
+public static <E extends Collection> boolean countPalindromes(E collection) {
+        ArrayList <String> sample = new ArrayList<>();
+        for (String word : sample) {
+            for (int i = 0; 1 < word.length(); i++) {
+                for (int j = word.length()-1; j < word.length(); j++) {
+                    if (word.charAt(i) == word.charAt(j)) {
+                        System.out.println(word.charAt(i));
+                        return true;
+                    } else
+                        break;
+                }
+            }
+
+        }
+        return false;
+}
 }
