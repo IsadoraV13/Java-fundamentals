b23f63bca2b1cd58abac7c893cea2a75cfba930e
IsadoraV13 <30869562+IsadoraV13@users.noreply.github.com>
2023-09-09 23:17:23 +0100

    First attempt at Interface - needs refining

--- src/labs_examples/interfaces/labs/InterfaceExercise_01.java
+++ src/labs_examples/interfaces/labs/InterfaceExercise_01.java
@@ -16,5 +16,104 @@ package labs_examples.interfaces.labs;
  *       - A PersonalDevice interface that is implemented by an iPad class and a Computer class. Two of the methods could be
  *          playMusic() and turnOff().
  */
+public class InterfaceExercise_01 {
+    public static void main(String[] args) {
+        Printer331 somePrinter = new Printer331();
+        System.out.println(somePrinter.getName() + " is printing: " + somePrinter.isOn(true));
+        somePrinter.turnOn();
+        somePrinter.turnOff();
+        System.out.println();
+
+        // check that we can pass false to 001 and isPrinting is will be false
+        Printer001 someOtherPrinter = new Printer001();
+        someOtherPrinter.turnOn();
+        someOtherPrinter.turnOff();
+    }
 
 
+    public interface printable {
+        boolean isOn(boolean isOn);
+
+        String getName();
+
+        void turnOn();
+
+        void turnOff();
+
+    }
+}
+
+
+class Printer001 implements InterfaceExercise_01.printable {
+
+    @Override
+    public boolean isOn(boolean isOn) {
+        return isOn;
+    }
+
+    @Override
+    public String getName() {
+        String name = "001";
+        return name;
+    }
+
+    @Override
+    public void turnOn() {
+        System.out.println("Model " + getName() + "has been turned on with the 'Start' button. ");
+        System.out.println(getName() + " is printing: " + isOn(true));
+    }
+
+    @Override
+    public void turnOff() {
+        System.out.println("Model " + getName() + "has been turned on with the 'Off' button. ");
+        System.out.println(getName() + " is printing: " + isOn(false));
+    }
+}
+
+class Printer301 implements InterfaceExercise_01.printable {
+
+    @Override
+    public boolean isOn(boolean isOn) {
+        return true; // is always on
+    }
+
+    @Override
+    public String getName() {
+        String name = "301";
+        return name;
+    }
+
+    @Override
+    public void turnOn() {
+        System.out.println("Model " + getName() + " has been activated by an item being sent for printing. ");
+    }
+
+    @Override
+    public void turnOff() {
+        System.out.println("Item printed. Model " + getName() + " is going to sleep. ");
+    }
+}
+
+
+class Printer331 extends Printer301 {
+
+    // Not an override but for variety: does not have its own isPrinting method but implements the parent one
+
+    @Override
+    public String getName() {
+        String name = "331";
+        return name;
+    }
+
+    @Override
+    public void turnOn() {
+        System.out.println("Model " + getName() + "has been activated by an item being sent for printing - and can " +
+                "be turned off by holding the 'Off' button for 3 seconds . ");
+    }
+
+    @Override
+    public void turnOff() {
+        System.out.println("Item printed. Off button held for 3 seconds. Model " + getName() + " going to sleep");
+        System.out.println(getName() + " is printing: " + super.isOn(false));
+    }
+}
